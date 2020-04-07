12d8af1dc14cef40583ce69ebdf391d4ae97e33d
Bradon Zhang <bradonzhang@gmail.com>
2020-04-07 03:20:06 -0400

    Added Android navigation bar back :(
    Encountered an issue where leaving the app and coming back would respawn the navigation bar, leading to less than desirable results for pages that rely on the screen dimensions being consistent.
    
    I added the navigation bar back for now. Hopefully there will be a fix for this in Expo sometime in the future.

--- src/assets/styles.tsx
+++ src/assets/styles.tsx
@@ -1,6 +1,6 @@
 import { Dimensions } from 'react-native';
 
-const { width: windowWidth, height: windowHeight } = Dimensions.get('screen');
+const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 
 const styles = {
   coinSize: windowWidth / 9,
